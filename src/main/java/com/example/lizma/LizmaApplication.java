204650435b094e837fe2a6041f14b99deebf6f18
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2024-08-21 02:03:32 +0300

    first commit

---
+++ src/main/java/com/example/lizma/LizmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lizma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LizmaApplication.class, args);
+    }
+
+}
